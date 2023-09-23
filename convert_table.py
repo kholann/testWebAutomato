ea4c64c47a02adba1c57dedeac2cd1cbe49b085f
Anna Bykova <kholann@yandex.ru>
2023-09-23 23:39:42 +0300

    Update convert_table.py

--- convert_table.py
+++ convert_table.py
@@ -6,36 +6,27 @@ from fuzzywuzzy import fuzz
 from langchain.embeddings import OpenAIEmbeddings
 from openai.embeddings_utils import cosine_similarity
 import json
-import numpy
 
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "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"
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def info_columns(table):
-    table = table + ".csv"
+def get_column_names(table):
     df = pd.read_csv(table)
     df_columns = df.columns.tolist()
-    name_file = 'columns_' + table.replace(".csv", ".txt")
-    with open(name_file, 'w') as wfile:
-        for i in range(0, len(df_columns)):
-            wfile.write(str(df_columns[i]) + "\n")
-    return name_file
+    return df_columns
 
-def find_in_dict(data, name_column):
+def find_name_column_in_dict_data(data, name_column):
     for key_value, value_value in data.items():
-        if str(name_column).strip() == str(value_value).strip():
+        if str(name_column).strip() == str(value_value).strip() or str(name_column).strip() == str(key_value).strip():
             return True
     return False
 
-def mapping_columns(template, source, mapping_file, target):
-    template = template + ".csv"
-    source = source + ".csv"
-    target = target + ".csv"
-
+def map_columns(template, source, mapping_file, target):
     df_template = pd.read_csv(template)
     template_columns = df_template.columns.tolist()
 
     df_source = pd.read_csv(source)
+    numb_rows_source = df_source.shape[0]
 
     template_dict_list = []
 
@@ -47,37 +38,59 @@ def mapping_columns(template, source, mapping_file, target):
                     if type(value) == list:
                         for j in range(0, len(value)):
                             if type(value[j]) == dict:
-                                if find_in_dict(value[j], template_columns[i]):
+                                if find_name_column_in_dict_data(value[j], template_columns[i]):
                                     template_dict_list.append(value[j])
             if type(data) == list:
                 for j in range(0, len(data)):
                     if type(data[j]) == dict:
-                        if find_in_dict(data[j], template_columns[i]):
+                        if find_name_column_in_dict_data(data[j], template_columns[i]):
                             template_dict_list.append(data[j])
 
     df_source_need = pd.DataFrame()
+
+    #TODO: work with mapping types
+    type_format_map = {}
+    type_format_map['integer'] = 'int'
+    type_format_map['string'] = 'str'
+
+    date_format_map = {}
+    date_format_map['dd-mm-yyyy'] = '%d-%m-%Y'
+    date_format_map['mm/dd/yyyy'] = '%m/%d/%Y'
+    date_format_map['yyyy-mm-dd'] = '%Y-%m-%d'
+
     for i in range(0, len(template_dict_list)):
         df_source_need[[template_dict_list[i]['template_column']]] = df_source[[template_dict_list[i]['source_column']]]
-        if (str(template_dict_list[i]['template_data_format']).strip().lower() != str(template_dict_list[i]['source_data_format']).strip().lower()):
+        template_data_format = str(template_dict_list[i]['template_data_format']).strip().lower()
+        source_data_format = str(template_dict_list[i]['source_data_format']).strip().lower()
+        template_column = template_dict_list[i]['template_column']
+        source_column = template_dict_list[i]['source_column']
+
+        if (template_data_format != source_data_format):
+            if find_name_column_in_dict_data(date_format_map, template_data_format):
+                if find_name_column_in_dict_data(date_format_map, source_data_format):
+                    df_source_need[template_column] = pd.to_datetime(df_source[source_column], format=date_format_map[source_data_format])
+                    df_source_need[template_column] = df_source_need[template_column].apply(lambda x: x.strftime(date_format_map[template_data_format]))
             try:
-                df_source_need[[template_dict_list[i]['template_column']]].astype(str(template_dict_list[i]['template_data_format']).lower())
+                map_format = type_format_map[template_data_format]
+                df_source_need[[template_column]].astype(map_format)
             except:
-                template_format = str(template_dict_list[i]['template_data_format']).lower()
-                source_format = str(template_dict_list[i]['source_data_format']).lower()
-                print(f'no format {template_format} or {source_format}')
+                print(f'No format {template_data_format} or {source_data_format}.')
+
+    numb_rows_target = df_source_need.shape[0]
+    if (numb_rows_source == numb_rows_target):
+        print(f'All data from {source} has been transferred correctly to {target}.')
+    else:
+        print(f'Not all data from {source} (number of rows {numb_rows_source}) has been transferred correctly to {target} (number of rows {numb_rows_target}).')
 
     df_source_need.to_csv(target)
 
-def find_similar_fuzz(template, source):
-    template = template + ".csv"
-    source = source + ".csv"
+def match_csv_file_columns_with_levenstain_dist(template, source):
+    column_mapping = template.replace(".txt", "") + "_" + source.replace(".txt", "") + "_ratio.csv"
 
-    writename = template.replace(".txt", "") + "_" + source.replace(".txt", "") + "_ratio.csv"
-
-    with open(template, 'r') as templateread, open(source, 'r') as sourceread, open(writename, 'w') as writefile:
-        writefile.write("template;source;ratio"+"\n")
-        template_read = templateread.readlines()
-        source_read = sourceread.readlines()
+    with open(column_mapping, 'w') as mapping_file:
+        mapping_file.write("template;source;ratio"+"\n")
+        template_read = get_column_names(template)
+        source_read = get_column_names(source)
         for i in range(0, len(template_read)):
             max_ratio = 0
             source_name_column = ""
@@ -86,31 +99,32 @@ def find_similar_fuzz(template, source):
                 if current_ratio > max_ratio:
                     max_ratio = current_ratio
                     source_name_column = source_read[j]
-            print(template_read[i], source_name_column, max_ratio)
-            writefile.write(str(template_read[i]).strip() + ";" + str(source_name_column).strip() + ";" + str(max_ratio) + "\n")
+            mapping_file.write(str(template_read[i]).strip() + ";" + str(source_name_column).strip() + ";" + str(max_ratio) + "\n")
 
-    return writename
+    return column_mapping
 
-def find_similar_openAI(template, source):
-    template = template + ".csv"
-    source = source + ".csv"
+def find_embeddings(template_read, source_read):
     embeddings = OpenAIEmbeddings()
-    writename = template.replace(".txt", "") + "_" + source.replace(".txt", "") + "_cos_sim.csv"
+    template_dict = {}
+    source_dict = {}
+    for i in range(0, len(template_read)):
+        template_embed = embeddings.embed_query(str(template_read[i]).strip())
+        template_dict[str(template_read[i]).strip()] = template_embed
 
-    with open(template, 'r') as templateread, open(source, 'r') as sourceread, open(writename, 'w') as writefile:
-        writefile.write("template;source;cos_sim" + "\n")
-        template_read = templateread.readlines()
-        source_read = sourceread.readlines()
-        template_dict = {}
-        source_dict = {}
-        for i in range(0, len(template_read)):
-            template_embed = embeddings.embed_query(str(template_read[i]).strip())
-            template_dict[str(template_read[i]).strip()] = template_embed
+    for j in range(0, len(source_read)):
+        source_embed = embeddings.embed_query(str(source_read[j]).strip())
+        source_dict[str(source_read[j]).strip()] = source_embed
 
-        for j in range(0, len(source_read)):
-            source_embed = embeddings.embed_query(str(source_read[j]).strip())
-            source_dict[str(source_read[j]).strip()] = source_embed
+    return template_dict, source_dict
 
+def match_csv_file_columns_with_cosine_sim(template, source):
+    column_mapping = template.replace(".txt", "") + "_" + source.replace(".txt", "") + "_cos_sim.csv"
+
+    with open(column_mapping, 'w') as mapping_file:
+        mapping_file.write("template;source;cos_sim" + "\n")
+        template_read = get_column_names(template)
+        source_read = get_column_names(source)
+        template_dict, source_dict = find_embeddings(template_read, source_read)
         for keytd, td in template_dict.items():
             max_ratio = 0
             source_name_column = ""
@@ -121,14 +135,11 @@ def find_similar_openAI(template, source):
                     source_name_column = keysd
 
             print(keytd, source_name_column, max_ratio)
-            writefile.write(keytd+ ";" + source_name_column + ";" + str(max_ratio) + "\n")
+            mapping_file.write(keytd + ";" + source_name_column + ";" + str(max_ratio) + "\n")
 
-    return writename
+    return column_mapping
 
-def ask_agent(template, source):
-    template = template + ".csv"
-    source = source + ".csv"
-
+def match_csv_file_columns_with_prompt(template, source):
     df_template = pd.read_csv(template)
     template_columns = df_template.columns.tolist()
     template_data = {}
@@ -149,8 +160,10 @@ def ask_agent(template, source):
         messages=[{"role":"user", "content": f"given following column names from the template set {template_data} find most similar column names out of the source set {source_data} "
                                              f"in case of ambiguous mapping, choose the first option "
                                              f"find for chosen column names from the template set {template_data} values from {template_data.values()} by keys = column name "
+                                             #TODO: return in python format
                                              f"and define the data format for the found values "
                                              f"find for chosen column names from the source set {source_data} values from {source_data.values()} by keys = column name "
+                                             #TODO: return in python format
                                              f"and define the data format for the found values "
                                              f"struct json for chosen column names and data format with structure: template_column: name of tempate column, template_data_format: data format of template column, source_column: name of source column, source_data_format: data format of source column. "
                                              f"Don't make any hints, give me result as json only"}
@@ -158,12 +171,11 @@ def ask_agent(template, source):
         )
     content = response['choices'][0].message.content
 
-    writename = template.replace(".csv", "") + "_" + source.replace(".csv", "") + "_model.txt"
-    with open(writename, "w") as wfile:
+    column_mapping = template.replace(".csv", "") + "_" + source.replace(".csv", "") + "_model.txt"
+    with open(column_mapping, "w") as wfile:
         wfile.write(str(content).strip())
 
-    return writename
-    #print(str(content).strip())
+    return column_mapping
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -174,10 +186,8 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if len(args.source) > 0 & len(args.template):
-        #template_model = ask_agent(str(args.template).strip(), str(args.source).strip())
-        #mapping_columns(str(args.template).strip(), str(args.source).strip(), template_model, str(args.target).strip())
-        mapping_columns(str(args.template).strip(), str(args.source).strip(), "template_table_A_model.txt", str(args.target).strip())
-        #template_file = info_columns(str(args.template).strip())
-        #source_file = info_columns(str(args.source).strip())
-        #ratio_file = find_similar_fuzz(template_file, source_file)
-        #find_similar_openAI(template_file, source_file)
+        template_model = match_csv_file_columns_with_prompt(str(args.template).strip(), str(args.source).strip())
+        map_columns(str(args.template).strip(), str(args.source).strip(), template_model, str(args.target).strip())
+        #map_columns(str(args.template).strip(), str(args.source).strip(), "template_table_B_model.txt", str(args.target).strip())
+        #ratio_file = match_csv_file_columns_with_levenstain_dist(str(args.template).strip(), str(args.source).strip())
+        #match_csv_file_columns_with_cosine_sim(str(args.template).strip(), str(args.source).strip())
